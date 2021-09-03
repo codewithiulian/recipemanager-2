e2ed5a1df5ad000d4068f0c0fbd553b9f4da3e00
Iulian Oana <iulian.andreioana@gmail.com>
2021-09-03 10:27:43 +0100

    Adding tutorial contents

---
+++ RecipeManager/Services/IRecipeService.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeManager.Services
+{
+    public interface IRecipeService
+    {
+        Recipe Create(Recipe recipe);
+        Recipe Get(int id);
+        List<Recipe> List();
+        Recipe Update(Recipe recipe);
+        void Delete(int id);
+    }
+}
